89020666da37a5692cb929a2594e32994397f0bf
tausvels <engr.tausifkhan@gmail.com>
2020-01-20 13:53:34 -0800

    added the dayListItem component and a format function to it. Also added an scss file for the styling.

---
+++ src/components/DayListItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classnames from 'classnames';
+
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayClass = classnames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full" : !props.spots
+  });
+  const formatSpots = function () {
+    const spot = props.spots;
+    if (spot > 1) {
+      return `${spot} spots remaining`
+    } else if (spot === 1) {
+      return `${spot} spot remaining`
+    } else {
+      return `no spots remaining`
+    }
+  }
+  return (
+    <li className={dayClass} onClick={() => {props.setDay(props.name)}}>
+      <h2 className="text-regular">{props.name}</h2> 
+      <h3 className="text-light">{formatSpots()}</h3>
+    </li>
+  )
+}
